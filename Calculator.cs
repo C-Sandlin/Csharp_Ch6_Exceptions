35c553b5d25bc83441ed6c50d060f94bdb21c194
Colin Sandlin <csandlin010@gmail.com>
2019-07-10 14:39:27 -0500

    finished

---
+++ Calculator.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Exceptions
+{
+
+    public class Calculator
+    {
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        // ...other methods (e.g. Add(), Subtract(), Multiply())...
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace exceptions
+namespace Exceptions
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Calculator calculator = new Calculator();
+                int answer = calculator.Divide(42, 0);
+                Console.WriteLine($"The answer is {answer}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Something went wrong! {ex.Message}");
+            }
         }
     }
 }
